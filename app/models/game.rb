c1f849bc496a0c2c33938e614290b2d136d82052
Saied Abbasi <saiedabbasijr@gmail.com>
2016-05-08 17:17:54 -0400

    Update game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -12,10 +12,6 @@ class Game < ActiveRecord::Base
     Keyword.each_keyword
   end
 
-
-
-
-  #platform id 43 = N64 #275 : SNES --> 9 #707 : NES --> 21 #683   ### 1665
   def self.persist_all_games 
     platforms = ['43','9','21'] 
     platforms.each do |platform_id|
